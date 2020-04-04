070499b4526439bd89c97b5b7820312fa7fd8aba
suman-shruti <63071088+suman-shruti@users.noreply.github.com>
2020-04-04 22:50:12 +0530

    Add files via upload

---
+++ Basic-Level2.py
@@ -0,0 +1,192 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+from random import randint
+print (randint(0,9))
+
+
+# In[1]:
+
+
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz=8888
+print(xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz)
+
+
+# In[3]:
+
+
+import keyword
+keyword.kwlist
+
+
+# In[12]:
+
+
+a=10
+type(a)
+
+
+# In[10]:
+
+
+b=10.5
+type(b)
+
+
+# In[13]:
+
+
+c=True
+type(c)
+
+
+# In[15]:
+
+
+a=10
+id(a)
+
+
+# In[16]:
+
+
+a=1233333334444444445555555555556666666666666666777777777777
+type(1233333334444444445555555555556666666666666666777777777777)
+
+
+# In[19]:
+
+
+a=0b1101
+print(a)
+
+
+# In[23]:
+
+
+a=0o125634
+print(a)
+
+
+# In[25]:
+
+
+a=0x17438642
+print(a)
+
+
+# In[26]:
+
+
+a=0xbad
+print(a)
+
+
+# In[27]:
+
+
+bin(0o123)
+
+
+# In[32]:
+
+
+hex(0o123456)
+
+
+# In[31]:
+
+
+oct(0xface)
+
+
+# In[34]:
+
+
+f=1.2e3
+print(f)
+
+
+# In[35]:
+
+
+x=10+20j
+print(type(x))
+
+
+# In[38]:
+
+
+x=0B1111+20j
+print(x)
+
+
+# In[41]:
+
+
+a=10
+b=20
+c=a>b
+print(c)
+
+
+# In[43]:
+
+
+print(True-False)
+
+
+# In[53]:
+
+
+a="""she
+is
+mad"""
+print(a)
+
+
+# In[55]:
+
+
+a='this is a "table" which is broken'
+print(a)
+
+
+# In[56]:
+
+
+a="""this 'is' a "table" which is broken"""
+print(a)
+
+
+# In[59]:
+
+
+s='abcdefghijklmnop'
+print(s[3:9]) #return string to 3 to 8 intex
+
+
+# In[64]:
+
+
+s='durga'
+output=s[0:4]+s[4].upper()
+print(output)
+
+
+# In[66]:
+
+
+s='durga'
+output=s[0].upper()+s[1:len(s)-1]+s[-1].upper()
+print(output)
+
+
+# In[ ]:
+
+
+
+
